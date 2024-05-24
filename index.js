4c6cc0624f876103b2176374ceb8cb07b843e6c3
sri <146051957+SrisailamKakurala@users.noreply.github.com>
2024-05-24 23:04:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+ origin: 'https://otp-1.netlify.app' 
+}));
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URL)
